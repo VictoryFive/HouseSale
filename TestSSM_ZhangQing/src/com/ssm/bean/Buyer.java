8546ccdad351e89d891fab81634393ba1665d327
ZhangQingJava06 <44408810+ZhangQingJava06@users.noreply.github.com>
2018-11-14 19:55:44 +0800

    第一次提交
    主要负责bean

---
+++ TestSSM_ZhangQing/src/com/ssm/bean/Buyer.java
@@ -0,0 +1,54 @@
+package com.ssm.bean;
+
+public class Buyer {
+	private Integer b_id;
+	private String b_username;
+	private String b_password;
+	private String b_gender;
+	private Integer b_age;
+	private String b_name;
+	private String b_telephone;
+	public Integer getB_id() {
+		return b_id;
+	}
+	public void setB_id(Integer b_id) {
+		this.b_id = b_id;
+	}
+	public String getB_username() {
+		return b_username;
+	}
+	public void setB_username(String b_username) {
+		this.b_username = b_username;
+	}
+	public String getB_password() {
+		return b_password;
+	}
+	public void setB_password(String b_password) {
+		this.b_password = b_password;
+	}
+	public String getB_gender() {
+		return b_gender;
+	}
+	public void setB_gender(String b_gender) {
+		this.b_gender = b_gender;
+	}
+	public Integer getB_age() {
+		return b_age;
+	}
+	public void setB_age(Integer b_age) {
+		this.b_age = b_age;
+	}
+	public String getB_name() {
+		return b_name;
+	}
+	public void setB_name(String b_name) {
+		this.b_name = b_name;
+	}
+	public String getB_telephone() {
+		return b_telephone;
+	}
+	public void setB_telephone(String b_telephone) {
+		this.b_telephone = b_telephone;
+	}
+	
+}
