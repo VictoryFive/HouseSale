7ee532f363e475fb3afece9c3633b4889f8a9f03
LiJiaShi <44409224+LiJiaShi@users.noreply.github.com>
2019-01-07 20:28:30 +0800

    修改houseDao

--- HouseSaleMain/src/com/ssm/dao/HouseDao.java
+++ HouseSaleMain/src/com/ssm/dao/HouseDao.java
@@ -30,6 +30,9 @@ public interface HouseDao {
 	
 	
 
+	List<House> find2();
+	List<House> find8(double a,double b);
+	List<House> find4(double a,double b);
 	List<House> find5(String a);
 	List<House> find6(double a1,double a2,double b1,double b2);
 	List<House> find7(double x,double y);
