f34d720273c607ea87f7cb3aed8e95d54cb8b67b
ZhangQingJava06 <44408810+ZhangQingJava06@users.noreply.github.com>
2019-01-08 09:42:11 +0800

    修改

--- HouseSaleMain/src/com/ssm/dao/UserDao.java
+++ HouseSaleMain/src/com/ssm/dao/UserDao.java
@@ -9,7 +9,6 @@ public interface UserDao {
 	List<Buyer> selectAllUser();
 	List<Buyer> selectBuyer(String username,String password);
 	List<Saler> selectSaler(String username,String password);
-	//¸üÐÂ£¬ÐÞ¸Ä£¬×¢²á
 	void saveBuyer(Buyer buyer);
 	void saveSaler(Saler saler);
 	
